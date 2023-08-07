d5c7b9dab3866a3407b75535f4c4d1714bcec27a
super-cool-ducky <130040504+super-cool-ducky@users.noreply.github.com>
2023-08-07 19:28:08 +1000

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,31 @@
+canvas = document.getElementById("myCanvas");
+color: "red";
+ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e)
+{
+    color = document.getElementById("color").ariaValueMax;
+    console.log(color);
+
+    mouse_x = e.clientX - canvas.offsetLeft;
+    mouse_y = e.clientY - canvas.offsetTop;
+
+    console.log("X = " + mouse_x + " ,Y = " + mouse_y);
+    CSSNumericValue(mouse_x , mouse_y);
+
+    function circle(mouse_x , mouse_y)
+    {
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = 2;
+        ctx.arc(200, 200, 40, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+}
